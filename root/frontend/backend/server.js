09fbb57612087707df3c9a07d12c96b9dae7c896
leonardo <l_b384@txstate.edu>
2023-03-07 23:13:13 -0600

    Intial Backend Implemented, MongoDB connected

---
+++ root/frontend/backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const app = express();
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));

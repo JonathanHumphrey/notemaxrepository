359c24edf4a0e25fc7cd94288fa2696dfb73b6f3
Adam McBay <99346794+Adam-McBay@users.noreply.github.com>
2023-04-04 18:34:51 -0500

    Update home style

--- root/frontend/front-end-hfg-main/src/pages/Home.js
+++ root/frontend/front-end-hfg-main/src/pages/Home.js
@@ -5,25 +5,29 @@ import { Link } from "react-router-dom";
 const Home = () => {
 	return (
 		<div className="container">
-			<div className="main-content">
+			<div className="rectangle">
+			</div>
+			<div className="text">
 				<h1 id="title">NoteMax</h1>
 				<p id="splash-text">
 					Lorem ipsum dolor sit amet consectetur. Aliquam nunc blandit et fusce
 					enim donec nunc cursus. Accumsan vivamus sit morbi id. Tristique
 					malesuada turpis vulputate porta.
 				</p>
+				</div>
+				
 				<div className="links">
 					<Link className="styled-link" to="/login">
-						Log In
+						<a>Log In</a>
 					</Link>
+					</div>
+					<div className="links">
 					<Link className="styled-link" to="/signup">
 						Sign Up
 					</Link>
-				</div>
+					
 			</div>
-			<div className="img-placeholder">
-				<p className="placeholder">image to be placed later</p>
-			</div>
+			
 		</div>
 	);
 };

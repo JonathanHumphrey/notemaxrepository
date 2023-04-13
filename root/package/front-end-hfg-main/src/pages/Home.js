e95cfbcfa9513172698988f3c64a84cce0790557
Adam McBay <99346794+Adam-McBay@users.noreply.github.com>
2023-04-12 19:25:30 -0500

    Update Home.js

--- root/package/front-end-hfg-main/src/pages/Home.js
+++ root/package/front-end-hfg-main/src/pages/Home.js
@@ -1,38 +1,37 @@
 import React from "react";
 import "../styles/Home.css";
 import { Link } from "react-router-dom";
-//<div className="rectangle"></div>
+
 const Home = () => {
 	return (
-		<div className="home-container">
-			<div className="rectangle"></div>
+		<div className="container">
+			<div className="rectangle">
+			</div>
 			<div className="text">
 				<h1 id="title">NoteMax</h1>
+				
+				
 				<p id="splash-text">
-					Welcome to NoteMax, the ultimate note-taking app for all your
-					productivity needs! Whether you're a student, professional, or just
-					someone who loves to jot down ideas, NoteMax is here to streamline
-					your note-taking experience like never before.
+					Welcome to NoteMax, the ultimate note-taking app for all your productivity needs! 
+					Whether you're a student, professional, or just someone who loves to jot down ideas, NoteMax is 
+					here to streamline your note-taking experience like never before.
 				</p>
+				</div>
+				
+				<div className="links">
+					<Link className="styled-link" to="/login">
+						<a>Log In</a>
+					</Link>
+					
+					<Link className="styled-link" to="/signup">
+						Sign Up
+					</Link>
+					
 			</div>
-
-			<div className="links">
-				<Link className="styled-link" to="/login">
-					Log In
-				</Link>
-				<Link className="styled-link" to="/signup">
-					Sign Up
-				</Link>
-			</div>
-
-			{/*<div className="text">
-				<h1 id="title">About Us</h1>
-				<p id="splash-text">
-					We a
-				</p>
-	</div>*/}
+			
 		</div>
 	);
 };
 
 export default Home;
+
